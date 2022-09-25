6432d7b938404f0923c72ec74d66e264a1314d38
2147118Joshua <joshua.stalin@mca.christuniversity.in>
2022-09-25 20:08:21 +0530

    .

---
+++ SharedPref/app/src/main/java/com/example/p4/SharedPrefModalClass.java
@@ -0,0 +1,14 @@
+package com.example.p4;
+
+public class SharedPrefModalClass {
+    public String name;
+    public int age;
+
+
+    public SharedPrefModalClass(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+}
